a8ebe07de0c47f769fc5c3a6c7a383d781c5f715
Адылканов Муратжан Талгатович <murka_505853@mail.ru>
2019-06-19 16:11:21 +0600

    Практика 31.05

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practice31._05
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            processesDataGrid.ItemsSource = Process.GetProcesses();
+        }
+
+        private void RemoveTaskButtonClick(object sender, RoutedEventArgs e)
+        {
+            foreach (var process in Process.GetProcesses())
+            {
+                if ((processesDataGrid.SelectedItem as Process).Id == process.Id)
+                {
+                    try
+                    {
+                        process.Kill();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Недоступно");
+                    }
+                    break;
+                }
+            }
+            processesDataGrid.ItemsSource = Process.GetProcesses();
+        }
+
+    }
+}
